1992532ee8ccd24fb0deb080d0131c2891e591c0
jumpert <juan.perezc@correo.ucu.edu.uy>
2023-11-25 05:35:42 -0300

    listo

--- app/src/main/java/com/example/marvel_app_final/view/AllCharactersFragment.kt
+++ app/src/main/java/com/example/marvel_app_final/view/AllCharactersFragment.kt
@@ -1,19 +1,22 @@
 package com.example.marvel_app_final.view
 
+import android.content.Context.MODE_PRIVATE
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
+import android.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import androidx.paging.PagingData
+import androidx.paging.filter
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.marvel_app_final.adapter.AllCharactersAdapter
 import com.example.marvel_app_final.databinding.FragmentAllCharactersBinding
@@ -21,6 +24,7 @@ import com.example.marvel_app_final.helper.NetworkChecker
 import com.example.marvel_app_final.model.character.Character
 import com.example.marvel_app_final.viewmodel.AllCharactersViewModel
 import com.example.marvel_app_final.viewmodel.SearchCharacterbyIdViewModel
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -38,26 +42,93 @@ class AllCharactersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentAllCharactersBinding.inflate(inflater, container, false)
-
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        // colocar el binding.searchview
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
+                //usar viewmodel allcharactersviewmodel
 
+                lifecycleScope.launch {
+                    binding.allCharactersRefresh.isRefreshing = false
+                    viewModel.getListData().collectLatest { pagingData ->
+                        if(query != null){
+                            allCharactersAdapter.submitData(pagingData.filter { it.name.startsWith(query) })
+                        } else {
+                            allCharactersAdapter.submitData(pagingData)
+                        }
+                    }
+                }
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
 
-                return true
+                lifecycleScope.launch {
+                    binding.allCharactersRefresh.isRefreshing = false
+                    viewModel.getListData().collectLatest { pagingData ->
+                        if(newText != null){
+                            allCharactersAdapter.submitData(pagingData.filter { it.name.startsWith(newText) })
+                        } else {
+                            allCharactersAdapter.submitData(pagingData)
+                        }
+
+                    }
+                }
+                return false
             }
-
         })
-
+        binding.logOutBtn.setOnClickListener {
+            signOut()
+            val directions = AllCharactersFragmentDirections
+                .actionAllCharactersFragmentToLoginFragment()
+            findNavController().navigate(directions)
+        }
 
         return binding.root
     }
 
     override fun onResume() {
         super.onResume()
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //usar viewmodel allcharactersviewmodel
+
+                lifecycleScope.launch {
+                    binding.allCharactersRefresh.isRefreshing = false
+                    viewModel.getListData().collectLatest { pagingData ->
+                        if (query != null) {
+                            allCharactersAdapter.submitData(pagingData.filter {
+                                it.name.startsWith(
+                                    query
+                                )
+                            })
+                        } else {
+                            allCharactersAdapter.submitData(pagingData)
+                        }
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                lifecycleScope.launch {
+                    binding.allCharactersRefresh.isRefreshing = false
+                    viewModel.getListData().collectLatest { pagingData ->
+                        if (newText != null ) {
+                            allCharactersAdapter.submitData(pagingData.filter {
+                                it.name.startsWith(
+                                    newText
+                                )
+                            })
+                        } else {
+                            allCharactersAdapter.submitData(pagingData)
+                        }
+
+                    }
+                }
+                return false
+            }
+        })
 
     }
 
@@ -69,10 +140,21 @@ class AllCharactersFragment : Fragment() {
         networkChecker.performActionIfConnected { initViewModel() }
     }
 
+    private fun signOut() {
+        // eliminar preferencias
+        val editor = binding.root.context.getSharedPreferences("sharedPrefs", MODE_PRIVATE).edit()
+        editor.putBoolean("isLogged", false)
+        editor.putString("username", "")
+        editor.apply()
+
+        // cerrar sesion
+        FirebaseAuth.getInstance().signOut()
+    }
+
     private fun initRecyclerView() {
         binding.charactersRecyclerView.apply {
             layoutManager = GridLayoutManager(context, 2)
-            setHasFixedSize(true)
+
             allCharactersAdapter = AllCharactersAdapter { character ->
                 val directions = AllCharactersFragmentDirections
                     .actionAllCharactersFragmentToCharacterDetailsFragment(character)
@@ -86,7 +168,6 @@ class AllCharactersFragment : Fragment() {
                 showEmptyList(isListEmpty)
 
                 binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
-
 
             }
         }
